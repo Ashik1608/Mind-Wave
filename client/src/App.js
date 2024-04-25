183e2bc9b9e2fc5033af4bdf87d9e3682db2fc06
kabilan2902 <kabilan.24it@licet.ac.in>
2024-04-26 00:52:09 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homepage";
 import LoginPage from "./scenes/loginpage";
 import ProfilePage from "./scenes/profilepage";
-<<<<<<< HEAD
 import VoiceChat from "./scenes/voicechat";
-import VrModule from "./scenes/VrModule";
-=======
-import VoiceChat from "./scenes/VoiceChat";
 import Counsellor from "./scenes/Counsellor";
->>>>>>> 53db27d26531717ec134b99f09092d5d1b6aeb24
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
